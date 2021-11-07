6a2eac50e2073cb018e6afe0904caf1b6663517e
djh95 <dengjiahui95@gmail.com>
2021-11-07 18:02:42 +0100

    fix bug

--- code/Word2Vec.py
+++ code/Word2Vec.py
@@ -12,13 +12,14 @@ def compute_sentence_list(tags_list):
         sl.append(s)
     return sl
 
-def compute_word_matrix(tags_list):
-    if os.path.isfile(Word2Vec_Model_Path):
-        model = Word2Vec.load(Word2Vec_Model_Path)
+def compute_word_matrix(tags_list, dimentions):
+    Path = Word2Vec_Model_Path + str(dimentions) + '_' + str(Word_Dimensions) + '.model'
+    if os.path.isfile(Path):
+        model = Word2Vec.load(Path)
     else:
         sentence_list = compute_sentence_list(tags_list)
         model = Word2Vec(sentences=sentence_list, vector_size=Word_Dimensions, window=5, min_count=1, workers=4)
-        model.save(Word2Vec_Model_Path)
+        model.save(Path)
     res = []
     for i in range(len(model.wv)):
         res.append(model.wv[str(i)].tolist())
