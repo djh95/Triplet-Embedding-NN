36b8612f5be0860bd8a7e617908a699877a8f747
djh95 <dengjiahui95@gmail.com>
2021-12-14 14:00:01 +0100

    add package.sh

---
+++ package.sh
@@ -0,0 +1,11 @@
+python 3.8
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
+pip install opencv-python
+conda install -c conda-forge notebook
+conda install -c conda-forge tensorboard
+pip install git+https://github.com/philferriere/cocoapi.git#subdirectory=PythonAPI
+pip install tqdm
+pip install GPUtil
+pip install jupyterplot
+pip install genism
+cp coco
